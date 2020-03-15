b17d8d74c2228df821643c12f4c76ffb7488428a
Tony Ramirez <39904525+Daiyuusha@users.noreply.github.com>
2020-03-15 16:33:41 +0800

    express requests

--- la sale/index.js
+++ la sale/index.js
@@ -13,6 +13,39 @@ app.get('/', function(req, res) {
     res.render('userlogin', {});
 });
 
+
+
+app.get('/browse', function(req, res) {
+    res.render('browselisting', {});
+});
+
+app.get('/createlisting', function(req, res) {
+    res.render('createlisting', {});
+});
+
+app.get('/editlisting', function(req, res) {
+    res.render('editlisting', {});
+});
+app.get('/editprofile', function(req, res) {
+    res.render('editprofile', {});
+});
+app.get('/listing', function(req, res) {
+    res.render('listing', {});
+});
+app.get('/messages', function(req, res) {
+    res.render('messages', {});
+});
+app.get('/mylistings', function(req, res) {
+    res.render('mylistings', {});
+});
+app.get('/profile', function(req, res) {
+    res.render('profile', {});
+});
+
+
+
+
+
 app.listen(port, function() {
     console.log('App listening at port ' + port);
 });
