87ce01846b28519b386176cbac2eaa53c224a806
Tony Ramirez <39904525+Daiyuusha@users.noreply.github.com>
2020-04-03 17:28:11 +0800

    database setup

---
+++ la sale/models/ArcherModel.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var ArcherSchema = new mongoose.Schema({
+    email: {
+        
+    },
+});
---
+++ la sale/models/db.js
@@ -0,0 +1,10 @@
+const mongoose = require(mongoose);
+
+// const User = req
+
+const url = 'mongodb://localhost:27017/lasaledb';
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
