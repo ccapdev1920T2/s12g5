1e7ab764b28433a067d2fdc9b1807a49dba08352
Trisha <48178740+trishapelagio@users.noreply.github.com>
2020-04-09 01:14:02 +0800

    navbar search

--- la sale/views/scripts/navbar_script.js
+++ la sale/views/scripts/navbar_script.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $("#search_nav").click(function() {
     	var item = $("#search_item").val()
     	console.log('hello')
-        window.location.replace("/searchnav?search=" + item)
+    	console.log(item)
+    	
+    	var link = "/searchnav?search="+item
+        window.location.replace(link)
     });
 })
