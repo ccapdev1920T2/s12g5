f96e28e5c6b8d58814673f60c8620e8ab5c7570c
Trisha <48178740+trishapelagio@users.noreply.github.com>
2020-04-09 01:46:23 +0800

    WEEEEEEEE WORKING NAVBAR SEARCH GOOD NIGHT PEOPLE :((

--- la sale/views/scripts/navbar_script.js
+++ la sale/views/scripts/navbar_script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#search_nav").click(function() {
     	var item = $("#search_item").val()
 
-        var url = "/searchnav?=" + item;
+        var url = "/searchnav?search=" + item;
         console.log(url);
         window.location.href=  url;
 
